7ec6fd67b66e5720c0922d841de80c735df8ff21
ivan <fanwenyue2006@126.com>
2019-08-20 15:50:14 +0800

    修改测试环境数据库地址

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -22,7 +22,7 @@ DATABASES = {
         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
         'PASSWORD': 'Aa123456',                        # 你的数据库password
-        'HOST': '172.50.100.232',                   		   # 数据库HOST
+        'HOST': '10.202.6.26',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
